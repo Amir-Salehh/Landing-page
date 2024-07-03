8ba8009b23da7d418f454c927f774b920fdb49c8
Amir-Salehh <amir.saleh.chess@gmail.com>
2024-07-03 09:30:01 +0330

    Update sign_up.php

--- actions/sign_up.php
+++ actions/sign_up.php
@@ -9,20 +9,25 @@ if(isset($_POST['signup'])){
         $phone_number = $_POST['phone'];
         $password = $_POST['password'];
         global $conn ;
-
-        // sql
-        $query = "INSERT INTO login SET userName=? , email=? , mobile=? , password=? " ;
-        // stmt
-        $stmt = $conn->prepare($query) ;
-        // bind
-        $stmt->bindValue(1, $username);
-        $stmt->bindValue(2, $email);
-        $stmt->bindValue(3, $phone_number);
-        $stmt->bindValue(4, $password);
-        // exe
-        $stmt->execute();
-        echo "account created";
-        header("location:../index.php");
+        
+        if($username == "" or $email == "" or $phone_number == "" or $password == ""){
+            header("location:../index.php?empty=ok") ;
+        }
+        else{
+            // sql
+            $query = "INSERT INTO login SET userName=? , email=? , mobile=? , password=? " ;
+            // stmt
+            $stmt = $conn->prepare($query) ;
+            // bind
+            $stmt->bindValue(1, $username);
+            $stmt->bindValue(2, $email);
+            $stmt->bindValue(3, $phone_number);
+            $stmt->bindValue(4, $password);
+            // exe
+            $stmt->execute();
+            echo "account created";
+            header("location:../index.php");
+        }
     }
     catch (PDOException $error){
         echo "errors: ".$error->getMessage() ;
