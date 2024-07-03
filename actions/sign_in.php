35e9109e84ad42ce31a96deae509a6e80a8f88ce
Amir-Salehh <amir.saleh.chess@gmail.com>
2024-07-03 09:40:12 +0330

    Update sign_in.php

--- actions/sign_in.php
+++ actions/sign_in.php
@@ -7,24 +7,28 @@ if (isset($_POST['sign_in'])){
         $key = $_POST['key'];
         $password = $_POST['password'] ;
         global $conn ;
-
-        // sql
-        $query = "SELECT * FROM login WHERE ( mobile= :key OR email= :key OR username= :key ) AND (password= :password)" ;
-        // stmt
-        $stmt = $conn->prepare($query) ;
-        // bind
-        $stmt->bindValue(":key",$key);
-        $stmt->bindValue(":password",$password);
-        // exe
-        $stmt->execute() ;
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        $has_user = $stmt->rowCount();
-        if($has_user){
-            header("location: ../index.php?findUser=ok");
+        if($key == "" or $password == ""){
+            header("location: ../index.php?sign-in-empty=ok");
         }
-
         else{
-            header("location: ../index.php?notUser=ok");
+            // sql
+            $query = "SELECT * FROM login WHERE ( mobile= :key OR email= :key OR username= :key ) AND (password= :password)" ;
+            // stmt
+            $stmt = $conn->prepare($query) ;
+            // bind
+            $stmt->bindValue(":key",$key);
+            $stmt->bindValue(":password",$password);
+            // exe
+            $stmt->execute() ;
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $has_user = $stmt->rowCount();
+            if($has_user){
+                header("location: ../index.php?findUser=ok");
+            }
+    
+            else{
+                header("location: ../index.php?notUser=ok");
+            }
         }
     }
     catch(PDOException $error){
